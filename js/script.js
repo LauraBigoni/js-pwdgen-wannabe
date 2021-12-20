ea991b7710776c5dd51b9bfbda4a4d2c93950286
Laura Bigoni <laurabigoni.92@gmail.com>
2021-12-20 14:27:58 +0100

    created repo

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log('JS OK');
+
+/* 
+Traccia:
+
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+Bonus:
+scrivere "LA tua password è nomecogonomecolorepreferito21"
+Abbellire la pagina dal punto di vista grafico a piacimento!
+*/
+
+// 1 - Chiedo il nome al'utente 
+// 2 - Salvo il nome in una variabile
+// 3 - Chiedo il cognome all'utente
+// 4 - Salvo il cognome in una variabile
+// 5 - Chiedo il colore preferito all'utente
+// 6 - Salvo il colore in una variabile
+// 7 - Recupero un elemento in pagina
+// 8 - Modifico il contenuto testuale
+
+
+// Chiedo il nome all'utente
